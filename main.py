c48b379babd2b1f029ea7421fe25520a7c89e53e
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-05-17 22:44:43 +0200

    [UPDATE] csv generation cleaned

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-from quart import Quart, request #, websocket
+from quart import Quart, request, send_file #, websocket
 from config.api import api_conf
 from src.data_process import process
-
+import json
 import pandas as pd
 import io
-    
+import uuid
 
 app = Quart(__name__)
+
+
+# https://colab.research.google.com/
+
 
 @app.route(f'/api/{api_conf["VERSION"]}/models/generator', methods=["POST"])
 async def model_generator():
     try:
-        test = await request.files
-        df = pd.read_csv(io.BytesIO(test["dataset"].read()), delimiter=",", encoding='utf-8')
+        csv_file = await request.files
+        df = pd.read_csv(io.BytesIO(csv_file["dataset"].read()), delimiter=",", encoding='utf-8')
 
-        # WIP
-        test = await process(df=df)
+        df_clean = await process(df=df)
 
-        return {"message":"df generated" }, 200
+        # Create an in-memory text stream
+        buf = io.BytesIO();
+        df_clean.to_csv(buf)
+
+        return await send_file(buf, attachment_filename=f'{uuid.uuid4().hex}.csv', as_attachment=True)
     except Exception as e:
         print(e)
         return {"message":"File error while parsing csv"}, 400
@@ -37,33 +44,3 @@ async def model_generator():
 if __name__ == "__main__":
     app.run()
 
-
-# https://colab.research.google.com/
-# import pandas as pd
-# import numpy as np
-
-# d = {'hobbiesName': ["football basketball", "volley football"], 'hobbiesDetails': ["souvent,desfois", "souvent,de temps en temps "]}
-# df = pd.DataFrame(data=d)
-
-# # Get dataframe interessting info
-# to_clear = ["hobbiesName", "hobbiesDetails"]
-
-# new_columns_hobbies_name = []
-# new_columns_hobbies_details = []
-
-# for i in range(df.index.start, df.index.stop):
-#    new_columns_hobbies_name = np.concatenate([new_columns_hobbies_name, df.loc[i]["hobbiesName"].split(" ")])
-#    new_columns_hobbies_details = np.concatenate([new_columns_hobbies_details, df.loc[i]["hobbiesDetails"].split(",")])
-
-# hobbies = list(set(new_columns_hobbies_name))
-# hobbies_details = list(set(new_columns_hobbies_details))
-
-# print(hobbies)
-# print(hobbies_details)
-
-# # Create a proper dataframe with interessting data
-
-
-
-# print("=========")
-# df
--- src/data_process.py
+++ src/data_process.py
@@ -1,4 +1,28 @@
 import asyncio
+import pandas as pd
+import numpy as np
 
 async def process(df):
-    return "ok"
+    to_clear = ["hobbiesName", "hobbiesDetails"]
+
+    df_tmp={}
+    for i in range(df.index.start, df.index.stop):
+       for idx, hobby in enumerate(df.loc[i]["hobbiesName"].split(" ")):
+            detail = df.loc[i]["hobbiesDetails"].split(",")[idx]
+            if ( hobby not in df_tmp ):
+                df_tmp[hobby] = [detail]
+            else:
+                df_tmp[hobby].append(detail)
+
+
+    # Give default None for empty
+    df_clean = pd.DataFrame.from_dict(df_tmp, orient='index').transpose()
+
+    for name_col_to_clear in to_clear:
+        df.pop(name_col_to_clear)
+
+    for col_name in df_clean:
+        df[col_name] = df_clean[col_name]
+
+    # Implement download CSV file
+    return df_clean
